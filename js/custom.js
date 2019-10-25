32e54bfffddbeea1fac3d9c439eacf248988d1e6
John wambua <wambuajohn020@gmail.com>
2019-10-25 17:22:39 +0300

    add custom.jss links

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-var akanNameMale =[
+var maleAkanName = [
     "Kwasi",
     "Kwadwo",
     "Kwabena",
@@ -6,9 +6,8 @@ var akanNameMale =[
     "Yaw",
     "Kofi",
     "Kwame"
-
-];
-var akanNameFemale = [
+  ];
+  var femaleAkanName = [
     "Akosua",
     "Adwoa",
     "Abenaa",
@@ -16,9 +15,8 @@ var akanNameFemale = [
     "Yaa",
     "Afua",
     "Ama"
-];
-
-var daysOfTheWeek = [
+  ];
+  var daysOfTheWeek = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -26,4 +24,67 @@ var daysOfTheWeek = [
     "Thursday",
     "Friday",
     "Saturday"
-];
+  ];
+  
+  //Hides bootstrap success button
+  document.getElementById("output").style.display = "none";
+  
+  //called when submit button is clicked
+  function onSubmit() {
+    var day = daysOfTheWeek[calculateDay()];
+    var name = getAkanName();
+    document.getElementById("day").innerHTML = day;
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("output").style.display = "block";
+  }
+  
+  function getYear() {
+    var year = document.getElementById("year").value;
+    return year;
+  }
+  
+  function getMonth() {
+    var month = document.getElementById("monthID").value;
+    return month;
+  }
+  function getDate() {
+    var date = document.getElementById("dateID").value;
+    return date;
+  }
+  function getGender() {
+    var gender = document.getElementsByName("gender");
+    for (i = 0; i < gender.length; i++) {
+      if (gender[i].checked) {
+        return gender[i].value;
+      }
+    }
+  }
+  
+  // calculate day of the week you were born
+  function calculateDay() {
+    var year = parseInt(getYear());
+    var MM = parseInt(getMonth());
+    var DD = parseInt(getDate());
+    var date = new Date(year + "/" + MM + "/" + DD);
+    var d = date.getDay();
+    return d;
+  }
+  
+  
+  //determine Akan name
+  function getAkanName() {
+    var gender = getGender();
+    var d = calculateDay();
+    var akanName = "";
+    if (gender === "male") {
+      akanName = maleAkanName[d];
+    } else {
+      akanName = femaleAkanName[d];
+    }
+    return akanName;
+  }
+  //function to clear form
+  function clearForm() {
+    document.getElementById("myform").reset();
+  }
+  
