c214c628780522aefc10aa5600938af00fedc6a9
John wambua <wambuajohn020@gmail.com>
2019-10-25 17:05:55 +0300

    add js link and arrays

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+var akanNameMale =[
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+
+];
+var akanNameFemale = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
+
+var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
